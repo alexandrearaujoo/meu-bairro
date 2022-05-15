c1f9da8b0e656c896e2977645d76cad38299939e
Alexandre Araujo <alexandre01393@gmail.com>
2022-05-15 19:45:04 -0300

    update: implemented carousel on galery component

--- src/components/Galery/index.tsx
+++ src/components/Galery/index.tsx
@@ -1,16 +1,37 @@
 import { SectionGalery, Title } from "./style";
+import Slider from "react-slick";
 
-const Galery = ({commerce}: any) => {
+const Galery = ({ commerce }: any) => {
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    initalSlide: 0,
+    pauseOnHover: true,
+    arrows: false,
+  };
+
   return (
-    <SectionGalery>
+    <>
       <Title>Galeria</Title>
-      <div>
-        <img src={commerce.image?.image1} alt="" />
-        <img src={commerce.image?.image2} alt="" />
-        <img src={commerce.image?.image3} alt="" />
-        <img src={commerce.image?.image4} alt="" />
-      </div>
-    </SectionGalery>
+      <Slider {...settings}>
+        <SectionGalery>
+          <img src={commerce.image?.image1} alt="" />
+        </SectionGalery>
+        <SectionGalery>
+          <img src={commerce.image?.image2} alt="" />
+        </SectionGalery>
+        <SectionGalery>
+          <img src={commerce.image?.image3} alt="" />
+        </SectionGalery>
+        <SectionGalery>
+          <img src={commerce.image?.image4} alt="" />
+        </SectionGalery>
+      </Slider>
+    </>
   );
 };
 
